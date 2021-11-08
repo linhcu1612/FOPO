61294f60e9cd07053d7da001f677a5a2d3492a3e
Nhat Linh <nhatlinh161232@gmail.com>
2021-11-08 11:50:06 +1100

    NotFound page new design

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,15 +1,20 @@
 /** @format */
 
 import React from "react";
-import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
-import { ROOT } from "../../routes/CONSTANTS";
+
+import classes from "./NotFound.module.css";
+import Tomato from "../../assets/logos/Tomato.png";
 
 const NotFound = () => {
   return (
     <>
-      <Link to={ROOT}>Home</Link>
-      <Typography variant='h2'>404: page not found!</Typography>
+      <div className={classes.number_wrapper}>
+        <h1 className={classes.number}>4</h1>
+        <img src={Tomato} alt='' className={classes.tomato} />
+        <h1 className={classes.number}>4</h1>
+      </div>
+      <h2 className={classes.error}>ERROR</h2>
+      <h3 className={classes.description}>Page Not Found</h3>
     </>
   );
 };
