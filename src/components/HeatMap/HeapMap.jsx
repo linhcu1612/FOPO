d5bf71795e1679cfaa0ab35f8e8b17e40128f60f
Nhat Linh <nhatlinh161232@gmail.com>
2021-12-27 14:00:26 +1100

    finish config for Report heapmap

---
+++ src/components/HeatMap/HeapMap.jsx
@@ -0,0 +1,54 @@
+/** @format */
+
+import React from "react";
+
+import moment from "moment";
+
+import ReactTooltip from "react-tooltip";
+
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
+
+const HeapMap = () => {
+  let startDate = moment().add(-364, "days");
+
+  let data = Array.from(new Array(365)).map((_, index) => {
+    return {
+      date: moment(startDate).add(index, "day"),
+      count: Math.floor(Math.random() * 4),
+    };
+  });
+
+  const getTooltipDataAttrs = (value) => {
+    if (!value || !value.date) {
+      return null;
+    }
+    return {
+      "data-tip": `${
+        value.count > 0 ? value.count : "No"
+      } pomodoros on ${value.date.format("MMM D, yyyy")}`,
+    };
+  };
+
+  return (
+    <>
+      <CalendarHeatmap
+        startDate={startDate}
+        endDate={moment()}
+        values={data}
+        showWeekdayLabels={true}
+        gutterSize={2}
+        tooltipDataAttrs={getTooltipDataAttrs}
+        classForValue={(value) => {
+          if (!value) {
+            return "color-empty";
+          }
+          return `color-scale-${value.count}`;
+        }}
+      />
+      <ReactTooltip />
+    </>
+  );
+};
+
+export default HeapMap;
--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -2,12 +2,17 @@
 
 import React from "react";
 
+import HeapMap from "../../components/HeatMap/HeapMap";
+
 import classes from "./Report.module.css";
 
 const Report = () => {
   return (
     <>
       <h1 className={classes.header}>Report</h1>
+      <div className={classes.container}>
+        <HeapMap />
+      </div>
     </>
   );
 };
