39d05ff4cc161548152299edb5ce9b08c7946411
linhcu1612 <nhatlinh161232@gmail.com>
2023-09-06 23:33:13 +1000

    set up config

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
-require("./routes/billingRoutes")(app);
-require("./routes/surveyRoutes")(app);
+// require("./routes/billingRoutes")(app);
+// require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
